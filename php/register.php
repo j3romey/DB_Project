f2a2dda559fc35d3640359723b305083ee75ad64
j3romey <jerome.adviento@hotmail.com>
2017-04-10 12:08:22 -0600

    sign up quick fix

--- php/register.php
+++ php/register.php
@@ -1,6 +1,9 @@
 <?php
-
-include 'db_login.php';
+ob_start();
+error_reporting(E_ALL); ini_set('display_errors', 'On');
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/php/db_login.php";
+include_once($path);
 
 
 $firstname = $_POST['firstname'];
@@ -14,8 +17,6 @@ if(!isset($firstname) || trim($firstname) == '')
 
 $username = $_POST['username'];
 $password = $_POST['password'];
-$uid = $_POST['UID'];
-
 
 $sql = "INSERT INTO user (Username, Password) 
         VALUES ('$username', '$password')";
@@ -26,16 +27,15 @@ if($result){
     $sql = "SELECT UID FROM user WHERE Username = '$username'";
     $result = mysqli_query($conn, $sql);
 
+    $row = $result->fetch_assoc();
+    $uid = $row["UID"];
+    echo $uid;
+    $sql = "INSERT INTO student (UID, Fname, Mname, Lname) VALUES ( '$uid', '$firstname', '$middlename', '$lastname')";
+    $result = mysqli_query($conn, $sql);
+    
     if($result){
         header("Location: /pages/status/success.html");
     }
-    
-    while($row = $result->fetch_assoc()){
-        $uid = $row["UID"];
-        $sql = "INSERT INTO student (UID, Fname, Mname, Lname) VALUES ( '$uid', '$firstname', '$middlename', '$lastname')";
-        $result = mysqli_query($conn, $sql);
-    }
-
     
 }else{
     header("Location: /pages/status/failure.html");
