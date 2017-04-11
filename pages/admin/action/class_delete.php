dbd64f7737d2bf11595939e31a6a4893eff78a8d
Albert Luu <fyrefawkes@me.com>
2017-04-10 23:37:37 -0600

    michael this is not how you use git

--- pages/admin/action/class_delete.php
+++ pages/admin/action/class_delete.php
@@ -1,87 +1,45 @@
 <?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/php/db_login.php";
+include_once($path);
 
-    error_reporting(E_ALL); ini_set('display_errors', 'On'); 
-    
-    ob_start();
-    $path = $_SERVER['DOCUMENT_ROOT'];
-    $path .= "/php/db_login.php";
-    include_once($path);
+$department = $_POST['department'];
+$number = $_POST['coursenumber'];
 
-    session_start();
-    $UCID = $_SESSION['student_id'];
 
-    // THESE ARE THE SECTION ID's
-    $lecture = $_POST['lecture'];
-    $tutorial = $_POST['tutorial'];
-    $lab = $_POST['lab'];
+$sql = "SELECT Cid from course WHERE Acronym = '$department' and Number = '$number'"; 
+$result = mysqli_query($conn, $sql);    
+$row = $result->fetch_assoc();
+$cid = $row['Cid'];
 
-    echo $lecture . " " . $lab . " " . $tutorial . "<BR>";
+$sql = "SELECT Sid from section WHERE Cid = '$cid'";
+$result = mysqli_query($conn, $sql);
+$row = $result->fetch_assoc();
 
-    echo '<h1>selected</h1>';
+foreach($row as $sid){
+    $sql = "DELETE FROM lecture 
+            WHERE Sid = '$sid'";
+    $result = mysqli_query($conn, $sql);
+    $sql = "DELETE FROM lab 
+            WHERE Sid = '$sid'";
+    $result = mysqli_query($conn, $sql);
+    $sql = "DELETE FROM tutorial 
+            WHERE Sid = '$sid'";
+    $result = mysqli_query($conn, $sql);
+} 
 
-    // grabs the selected LECTURE
-    $sql = "SELECT * from section WHERE Sid = '$lecture' "; 
-    $result = mysqli_query($conn, $sql);    
-    $row = $result->fetch_assoc();
-    echo $row['Room'] . " " . $row['Day'] . " " . $row['Time'] . "<BR>";
+$sql = "DELETE FROM section 
+        WHERE Cid = '$cid'";
+        
+$result = mysqli_query($conn, $sql);    
 
-    // grabs the selected TUTORIAL
-    $sql = "SELECT * from section WHERE Sid = '$tutorial' "; 
-    $result = mysqli_query($conn, $sql);    
-    $row = $result->fetch_assoc();
-    echo $row['Room'] . " " . $row['Day'] . " " . $row['Time'] . "<BR>";
+$sql = "DELETE FROM course 
+        WHERE Cid = '$cid'";
+        
+$result = mysqli_query($conn, $sql);
 
-    // grabs the selected LAB
-    $sql = "SELECT * from section WHERE Sid = '$lab' "; 
-    $result = mysqli_query($conn, $sql);    
-    $row = $result->fetch_assoc();
-    echo $row['Room'] . " " . $row['Day'] . " " . $row['Time'] . "<BR>";
-
-    // INSERT LECTURE
-    if(isset($lecture)){
-        $sql = "INSERT INTO shopping_cart (UCID, SID)  VALUES ('$UCID', '$lecture')";
-        $result = mysqli_query($conn, $sql); 
-        echo "there is lecture <BR>";
-        if(!$result){
-            // pop up errorr
-            echo ("<SCRIPT LANGUAGE='JavaScript'>
-            window.alert('Something Went Wrong')
-            window.location.href='/pages/student/student_enroll.php';
-            </SCRIPT>");
-            exit();
-        }
-    }
-    // INSERT LECTURE
-    if(isset($tutorial)){
-        $sql = "INSERT INTO shopping_cart (UCID, SID)  VALUES ('$UCID', '$tutorial')";
-        $result = mysqli_query($conn, $sql); 
-        echo "there is tutorial <BR>";
-
-        if(!$result){
-            echo ("<SCRIPT LANGUAGE='JavaScript'>
-            window.alert('Something Went Wrong')
-            window.location.href='/pages/student/student_enroll.php';
-            </SCRIPT>");
-            exit();
-        }
-    }
-    // INSERT LECTURE
-    if(isset($lab)){
-        $sql = "INSERT INTO shopping_cart (UCID, SID)  VALUES ('$UCID', '$lab')";
-        $result = mysqli_query($conn, $sql); 
-        echo "there is lab <BR>";
-        if(!$result){
-            echo ("<SCRIPT LANGUAGE='JavaScript'>
-            window.alert('Something Went Wrong')
-            window.location.href='/pages/student/student_enroll.php';
-            </SCRIPT>");
-            exit();
-        }
-    }
-
-    // pop up when everything query's properly
-    echo ("<SCRIPT LANGUAGE='JavaScript'>
-    window.alert('Succesfully Updated')
-    window.location.href='/pages/student/student_enroll.php';
+echo ("<SCRIPT LANGUAGE='JavaScript'>
+    window.alert('Succesfully deleted the course')
+    window.location.href='/pages/admin/admin_del.php';
     </SCRIPT>");
 ?>
--- pages/admin/admin_del.php
+++ pages/admin/admin_del.php
@@ -5,7 +5,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="/css/index.css">
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-<script type="text/javascript" src="action/admin_dd_menu.js"></script>
 
 <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 
@@ -21,14 +20,15 @@
     <h1> DELETE COURSES </h1>
     <div class = "del">
         <h3> PICK A COURSE </h3>
-        <select class="courses">
-            <option selected="selected">--Select Course--</option>
+        <form method="post" action="action/class_delete.php">
+        <select name="department">
+            <option selected="selected">--Select Department--</option>
             <?php
                 $path = $_SERVER['DOCUMENT_ROOT'];
                 $path .= "/php/db_login.php";
                 include_once($path);
             
-                $sql = "SELECT DISTINCT Acronym from course GROUP BY Acronym"; 
+                $sql = "SELECT DISTINCT Acronym from department GROUP BY Acronym"; 
             
                 $result = mysqli_query($conn, $sql);
             
@@ -36,14 +36,10 @@
                     echo "<option value=" . $row['Acronym'] . ">" . $row['Acronym'] . "</option>";
                 }   
             ?>
-        </select> <br><br>
-        <select class="number">
-            <option selected="selected">--Select Course--</option>
-        </select> <br><br>
-    
-        <form method="post" class="sections" action="action/class_delete.php">
-            
-        </form>
+        </select><br>
+        <input type="text" name="coursenumber" placeholder="Course Number" required><br>
+        <button type="submit">Delete Course</button>
+    </form>
     </div>
 </body>
 </html>
